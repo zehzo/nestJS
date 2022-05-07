b2b3cdcbfff3fd206945ad7f737e5af35afe499e
José Edson <edson.jr99@gmail.com>
2022-05-07 17:32:02 -0300

    criando regras de negocio para cadastrar um jogador

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { JogadoresService } from './jogadores.service';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
+  constructor(private readonly jogadoresService: JogadoresService) {}
+
   @Post()
   async criarAtualizarJogador(@Body() criaJogadorDto: CriarJogadorDto) {
-    const { email } = criaJogadorDto;
-    return JSON.stringify(`{
-      "nome": ${email}
-    }`);
+    await this.jogadoresService.criarAtualizarJogador(criaJogadorDto);
   }
 }
