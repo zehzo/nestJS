438fddcac5f85e46382f5a2dd6fe9414c7851036
zehzo <edson.ferreiraj@hotmail.com>
2022-05-09 23:26:56 -0300

    criando conexao com o mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    JogadoresModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:JRTG9VkR3p993V7j@cluster0.fitqh.mongodb.net/smartranking?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+    ),
+  ],
   controllers: [],
   providers: [],
 })
