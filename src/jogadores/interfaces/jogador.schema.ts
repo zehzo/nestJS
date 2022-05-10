7569974dbfa6b15898fe7047e6ca9b6f8e86aec7
zehzo <edson.ferreiraj@hotmail.com>
2022-05-10 00:08:41 -0300

    importando jogadorSchema no module

--- src/jogadores/interfaces/jogador.schema.ts
+++ src/jogadores/interfaces/jogador.schema.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose';
 
-export const jogadorSchema = new mongoose.Schema(
+export const JogadorSchema = new mongoose.Schema(
   {
     telefoneCelular: { type: String, unique: true },
     email: { type: String, unique: true },
